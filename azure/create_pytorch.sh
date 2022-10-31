fc33746ed8481fd777be1f7ae6f7801619455599
ChenggongWang <c.wang@princeton.edu>
2022-10-31 14:31:32 -0400

    cuda 11.7

--- azure/create_pytorch.sh
+++ azure/create_pytorch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+  
 ###################################################################
 #Script Name : create_pytorch.sh
 #Description : create env for pytorch
@@ -16,12 +16,12 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 # command starts here
 
-conda create -y -n cg_pytorch python=3.10 numpy jupyterlab
+conda create -y -n cg_pytorch python=3.9 numpy
 conda activate cg_pytorch
-conda install -y pytorch torchvision torchaudio cudatoolkit=11.6 -c pytorch -c conda-forge
+conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 conda install -y -c conda-forge matplotlib=3 cartopy scipy scikit-learn numba ipykernel
 conda install -y -c conda-forge xarray netcdf4
-python -m ipykernel install --user --name pytorch
+python -m ipykernel install --user --name cg_pytorch
 conda deactivate
 
 echo 'conda enviroment for pytorch created!'
